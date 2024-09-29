6916ff5f5ee9ffdcc4253047f31a83e4c60ead0b
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2024-09-29 23:13:53 +0530

    Add FontSans

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
-import { IBM_Plex_Sans } from "next/font/google";
-import "./globals.css";
+import { IBM_Plex_Sans as FontSans } from "next/font/google";
 
-const fontSans = IBM_Plex_Sans({
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
   subsets: ["latin"],
   weight: ["300", "400", "500", "700"],
+  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
@@ -19,7 +22,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={fontSans.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
